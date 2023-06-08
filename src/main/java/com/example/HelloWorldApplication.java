a7f5c75d3cf51371ee83df4193d8b9c7515268ac
Tausif Ahmad <82999506+ahmadtausif38@users.noreply.github.com>
2023-06-08 15:31:54 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/example/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorldApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 		System.out.println("This is sample SpringBoot Project for testing purpose...");
 	}
+	
+	@GetMapping("/test")
+	public String show(){
+		return "Hello!, Your first SpringBoot Project is deployed successfully...!"
 
+		}
 }
